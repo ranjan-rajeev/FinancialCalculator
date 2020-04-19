b8349c8ab779eeba96417a8f9cabc35b3dc4eb18
Rajeev Ranjan <rranjan1@tataunistore.com>
2020-04-19 14:35:35 +0530

    Added expression calcualtion

--- app/src/main/java/com/financialcalculator/utility/Util.java
+++ app/src/main/java/com/financialcalculator/utility/Util.java
@@ -2,17 +2,23 @@ package com.financialcalculator.utility;
 
 import com.financialcalculator.R;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Random;
+import java.util.Stack;
 
 /**
  * Created by Rajeev Ranjan -  ABPB on 05-04-2019.
  */
 public class Util {
 
+    public static final int PRECISION = 12;
 
     public static long getLongDate(String dateString) {
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd-MM-yyyy");
@@ -101,4 +107,158 @@ public class Util {
         }
     }
 
+    public static BigDecimal evaluate(String expression, HashMap<Character, BigDecimal> bigDecimals) {
+        char[] tokens = expression.toCharArray();
+
+        // Stack for numbers: 'values'
+        Stack<BigDecimal> values = new Stack<BigDecimal>();
+
+        // Stack for Operators: 'ops'
+        Stack<Character> ops = new Stack<Character>();
+
+        for (int i = 0; i < tokens.length; i++) {
+            // Current token is a whitespace, skip it
+            if (tokens[i] == ' ')
+                continue;
+
+            // current token is a variable ,fetch from hashmap , push it to stack for numbers
+            if (tokens[i] >= 'a' && tokens[i] <= 'z') {
+                values.push(bigDecimals.get(tokens[i]));
+            }
+            // Current token is a number, push it to stack for numbers
+            else if ((tokens[i] == '.') || ((tokens[i] >= '0' && tokens[i] <= '9'))) {
+                StringBuffer sbuf = new StringBuffer();
+                // There may be more than one digits in number
+                while (i < tokens.length && (tokens[i] == '.' || (tokens[i] >= '0' && tokens[i] <= '9'))) {
+                    sbuf.append(tokens[i++]);
+                }
+                i--; // to handle space as it moved one char ahead to check for int character
+                values.push(new BigDecimal(sbuf.toString()));
+            }
+
+            // Current token is an opening brace, push it to 'ops'
+            else if (tokens[i] == '(')
+                ops.push(tokens[i]);
+
+                // Closing brace encountered, solve entire brace
+            else if (tokens[i] == ')') {
+                while (ops.peek() != '(')
+                    values.push(applyOp(ops.pop(), values.pop(), values.pop()));
+                ops.pop();
+            }
+
+            // Current token is an operator.
+            else if (tokens[i] == '+' || tokens[i] == '-' ||
+                    tokens[i] == '*' || tokens[i] == '/' || tokens[i] == '^') {
+                // While top of 'ops' has same or greater precedence to current
+                // token, which is an operator. Apply operator on top of 'ops'
+                // to top two elements in values stack
+                while (!ops.empty() && hasPrecedence(tokens[i], ops.peek()))
+                    values.push(applyOp(ops.pop(), values.pop(), values.pop()));
+
+                // Push current token to 'ops'.
+                ops.push(tokens[i]);
+            }
+        }
+
+        // Entire expression has been parsed at this point, apply remaining
+        // ops to remaining values
+        while (!ops.empty())
+            values.push(applyOp(ops.pop(), values.pop(), values.pop()));
+
+        // Top of 'values' contains result, return it
+
+        return PRECISION == 0 ? values.pop() : values.pop().setScale(PRECISION, BigDecimal.ROUND_HALF_UP);
+    }
+
+    public static BigDecimal evaluate(String expression) {
+        char[] tokens = expression.toCharArray();
+
+        // Stack for numbers: 'values'
+        Stack<BigDecimal> values = new Stack<BigDecimal>();
+
+        // Stack for Operators: 'ops'
+        Stack<Character> ops = new Stack<Character>();
+
+        for (int i = 0; i < tokens.length; i++) {
+            // Current token is a whitespace, skip it
+            if (tokens[i] == ' ')
+                continue;
+
+            // Current token is a number, push it to stack for numbers
+            if (tokens[i] == '.' || (tokens[i] >= '0' && tokens[i] <= '9')) {
+                StringBuffer sbuf = new StringBuffer();
+                // There may be more than one digits in number
+                while (i < tokens.length && (tokens[i] == '.' || (tokens[i] >= '0' && tokens[i] <= '9'))) {
+                    sbuf.append(tokens[i++]);
+                }
+                i--; // to handle space as it moved one char ahead to check for int character
+                values.push(new BigDecimal(sbuf.toString()));
+            }
+
+            // Current token is an opening brace, push it to 'ops'
+            else if (tokens[i] == '(')
+                ops.push(tokens[i]);
+
+                // Closing brace encountered, solve entire brace
+            else if (tokens[i] == ')') {
+                while (ops.peek() != '(')
+                    values.push(applyOp(ops.pop(), values.pop(), values.pop()));
+                ops.pop();
+            }
+
+            // Current token is an operator.
+            else if (tokens[i] == '+' || tokens[i] == '-' ||
+                    tokens[i] == '*' || tokens[i] == '/' || tokens[i] == '^') {
+                // While top of 'ops' has same or greater precedence to current
+                // token, which is an operator. Apply operator on top of 'ops'
+                // to top two elements in values stack
+                while (!ops.empty() && hasPrecedence(tokens[i], ops.peek()))
+                    values.push(applyOp(ops.pop(), values.pop(), values.pop()));
+
+                // Push current token to 'ops'.
+                ops.push(tokens[i]);
+            }
+        }
+
+        // Entire expression has been parsed at this point, apply remaining
+        // ops to remaining values
+        while (!ops.empty())
+            values.push(applyOp(ops.pop(), values.pop(), values.pop()));
+
+        // Top of 'values' contains result, return it
+
+        return PRECISION == 0 ? values.pop() : values.pop().setScale(PRECISION, BigDecimal.ROUND_HALF_UP);
+    }
+
+    // Returns true if 'op2' has higher or same precedence as 'op1',
+    // otherwise returns false.
+    public static boolean hasPrecedence(char op1, char op2) {
+        if (op2 == '(' || op2 == ')')
+            return false;
+        if ((op1 == '*' || op1 == '/') && (op2 == '+' || op2 == '-'))
+            return false;
+        if ((op1 == '^') && (op2 == '*' || op2 == '/' || op2 == '+' || op2 == '-'))
+            return false;
+        else
+            return true;
+    }
+
+    // A utility method to apply an operator 'op' on operands 'a'
+    // and 'b'. Return the result.
+    public static BigDecimal applyOp(char op, BigDecimal b, BigDecimal a) {
+        switch (op) {
+            case '+':
+                return a.add(b);
+            case '-':
+                return a.subtract(b);
+            case '*':
+                return a.multiply(b);
+            case '/':
+                return a.divide(b, PRECISION, RoundingMode.HALF_UP);
+            case '^':
+                return a.pow(b.intValue()).setScale(PRECISION, RoundingMode.HALF_UP);
+        }
+        return new BigDecimal(0);
+    }
 }
--- app/src/test/java/com/financialcalculator/ExampleUnitTest.java
+++ app/src/test/java/com/financialcalculator/ExampleUnitTest.java
@@ -1,6 +1,13 @@
 package com.financialcalculator;
 
+import com.financialcalculator.utility.Util;
+
 import org.junit.Test;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -13,5 +20,48 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void evaluate_correct_expression() throws Exception {
+        assertEquals(1024.976, Util.evaluate("( 1 ^ 10 )"));
+    }
+
+    @Test
+    public void evaluate_withvalues_correct_expression() {
+        HashMap<Character, BigDecimal> decimalHashMap = new HashMap<>();
+        decimalHashMap.put('r', new BigDecimal(0.0066667).setScale(Util.PRECISION, BigDecimal.ROUND_HALF_UP));
+        decimalHashMap.put('m', new BigDecimal(850000).setScale(Util.PRECISION, BigDecimal.ROUND_HALF_UP));
+        decimalHashMap.put('t', new BigDecimal(420).setScale(Util.PRECISION, BigDecimal.ROUND_HALF_UP));
+        decimalHashMap.put('c', new BigDecimal(1).setScale(Util.PRECISION, BigDecimal.ROUND_HALF_UP));
+
+
+        String formulae = "(m*r) / ( (c+r) * (((c+r)^t)-c) )";
+        String formulae1 = "(m*r)";
+        String formulae2 = "(((c+r)^t)-c)";
+        String formulae3 = "( (c+r) * (((c+r)^n)-c) )";
+        String formulae4 = "(m*7.89/1200) / ( (1+7.89/1200) * (( (1+7.89/1200)^t)-1) )";
+
+
+       /* //String formulae = " ( m * r ) / ( c + r ) * ( ( ( c + r ) ^ n ) -  c)";
+        //String formulae = " ( m * r ) / ( ( c + r ) * ( ( ( c + r ) ^ n ) -  c ) )";
+
+
+        String formulae1 = "(100+9/3)";
+        String formulae2 = " ( ( 1 + 10 ) * ( ( ( 1 + 10 ) ^ 1 ) -  1 ) )";
+        String formulae3 = "(100*10)/((1+10)*(((1+10)^1)-1))";
+
+
+        String formulae5 = "((1+10)*(((1+10)^1)-1))";
+        // assertEquals(1024.976, Util.evaluate(formulae, decimalHashMap));
+        //assertEquals(new BigDecimal(1000).setScale(2, BigDecimal.ROUND_HALF_UP), Util.evaluate(formulae1));
+        //assertEquals(new BigDecimal(110).setScale(2,BigDecimal.ROUND_HALF_UP), Util.evaluate(formulae2));
+        //assertEquals(new BigDecimal(110).setScale(2,BigDecimal.ROUND_HALF_UP), Util.evaluate(formulae3));
+        assertEquals(new BigDecimal(110).setScale(2, BigDecimal.ROUND_HALF_UP), Util.evaluate(formulae5));
+        assertEquals(new BigDecimal(110).setScale(2, BigDecimal.ROUND_HALF_UP), Util.evaluate(formulae3));*/
+
+        //assertEquals(new BigDecimal(14000).setScale(Util.PRECISION, BigDecimal.ROUND_HALF_UP), Util.evaluate(formulae1, decimalHashMap));
+        //assertEquals(new BigDecimal(532.58).setScale(Util.PRECISION, BigDecimal.ROUND_HALF_UP), Util.evaluate(formulae, decimalHashMap));
+        assertEquals(new BigDecimal(532.58).setScale(Util.PRECISION, BigDecimal.ROUND_HALF_UP), Util.evaluate(formulae4, decimalHashMap));
     }
 }
